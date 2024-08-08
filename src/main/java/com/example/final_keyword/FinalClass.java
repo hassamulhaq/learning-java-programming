cdeff4c07c13bee5e3ac13c17d7eae8681f833b9
hassamulhaq <developer.hassam@gmail.com>
2024-08-08 20:08:29 +0500

    OOP final keyword

---
+++ src/main/java/com/example/final_keyword/FinalClass.java
@@ -0,0 +1,15 @@
+package com.example.final_keyword;
+
+final class FinalClass {
+    public void display() {
+        System.out.println("Hello World");
+    }
+}
+
+class Main2 extends FinalClass {
+    int z = 0;
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/example/final_keyword/FinalMethod.java
@@ -0,0 +1,23 @@
+package com.example.final_keyword;
+
+public class FinalMethod {
+
+    public final void display() {
+        System.out.println("display()");
+    }
+}
+
+class Main extends FinalMethod {
+
+    /**
+     * java: display() in com.example.final_keyword.Main cannot override display() in com.example.final_keyword.FinalMethod
+     * overridden method is final
+     */
+    public void display() {
+        System.out.println("ex: display()");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
